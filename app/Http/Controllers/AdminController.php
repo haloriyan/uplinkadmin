db59ba3b3b2db7cdd5222e3a4fc74c6ed2f06af9
Riyan Satria <riyan.satria.619@gmail.com>
2022-04-19 21:02:13 +0700

    Updatean

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -20,7 +20,7 @@ class AdminController extends Controller
 {
     public function me($getMessage = true) {
         $myData = Auth::guard('admin')->user();
-        $myData->messages = [];
+        $myData->messages = new \stdClass();
         if ($getMessage) {
             $myData->messages = ContactMessage::where('has_read', 0)->orderBy('created_at', 'DESC')->get();
         }
